3c0a732bf276f90fd16499b6486467c789142568
Niels Göttsch <ng@ife.de>
2024-07-23 21:25:28 +0200

    [fix] string cosmetics and formating

--- sale_order_batch/models/sale_order_batch_product.py
+++ sale_order_batch/models/sale_order_batch_product.py
@@ -26,7 +26,7 @@ class SaleOrderBatchProduct(models.Model):
         "sale.order.line", compute="_compute_sale_order_line_ids", store=True, readonly=False
     )
     product_uom_category_id = fields.Many2one(related="product_id.uom_id.category_id", depends=["product_id"])
-    product_uom_qty = fields.Float(compute="_compute_uom_qty")
+    product_uom_qty = fields.Float(compute="_compute_uom_qty", string="Quantity")
     product_uom = fields.Many2one(related="product_id.uom_id")
     product_packaging_id = fields.Many2one("product.packaging")
     product_packaging_qty = fields.Float(compute="_compute_product_packaging_qty")
@@ -38,7 +38,7 @@ class SaleOrderBatchProduct(models.Model):
                 lambda o: o.product_id == product.product_id
             )
 
-    @api.depends("batch_id.sale_order_line_ids")
+    @api.depends("sale_order_line_ids.product_uom_qty")
     def _compute_uom_qty(self):
         for product in self:
             product.product_uom_qty = sum(product.sale_order_line_ids.mapped("product_uom_qty"))
