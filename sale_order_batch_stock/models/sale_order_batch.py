8ac4b83b2fdce962bbef67ed4c607504b30aa501
Niels Göttsch <ng@ife.de>
2025-07-24 08:06:28 +0200

    [chore] update pre-commits

--- sale_order_batch_stock/models/sale_order_batch.py
+++ sale_order_batch_stock/models/sale_order_batch.py
@@ -7,8 +7,12 @@ STATES = [("open", "Open"), ("close", "Close")]
 class SaleOrderBatch(models.Model):
     _inherit = "sale.order.batch"
 
-    picking_ids = fields.Many2many("stock.picking", compute="_compute_picking_ids", string="Transfers")
-    delivery_count = fields.Integer(string="Delivery Orders", compute="_compute_picking_ids")
+    picking_ids = fields.Many2many(
+        "stock.picking", compute="_compute_picking_ids", string="Transfers"
+    )
+    delivery_count = fields.Integer(
+        string="Delivery Orders", compute="_compute_picking_ids"
+    )
 
     @api.depends("sale_order_ids.picking_ids")
     def _compute_picking_ids(self):
