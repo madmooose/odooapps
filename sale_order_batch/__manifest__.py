b281812874ad1f233c0f28a2039b841e02775182
Niels Göttsch <niels@ziemlichoptimal.de>
2026-05-28 10:30:24 +0200

    [FIX] sale_order_batch: unique ids for confirm button

--- sale_order_batch/__manifest__.py
+++ sale_order_batch/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "BAKEUP,Niels Göttsch",
     "website": "https://ziemlichoptimal.de",
     "category": "Sale",
-    "version": "16.0.2.3.1",
+    "version": "16.0.2.3.2",
     "depends": ["sale", "product"],
     "data": [
         "security/ir.model.access.csv",
